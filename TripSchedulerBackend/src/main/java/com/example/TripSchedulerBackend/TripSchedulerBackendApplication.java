1f1fbaf68e4344dd62334ea5d76db53ebf42d51f
rroshdy11 <rroshdy75@gmail.com>
2022-11-04 17:26:30 +0200

    First Commit Empty Backend With Configured DB

---
+++ TripSchedulerBackend/src/main/java/com/example/TripSchedulerBackend/TripSchedulerBackendApplication.java
@@ -0,0 +1,16 @@
+package com.example.TripSchedulerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class TripSchedulerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripSchedulerBackendApplication.class, args);
+	}
+
+
+
+}
---
+++ TripSchedulerBackend/src/test/java/com/example/TripSchedulerBackend/TripSchedulerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TripSchedulerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripSchedulerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
